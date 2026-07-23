9f5d211538e28a9e576807b953132a721ad92dae
Chetanya Moudgil <chetanya.moudgil@mail.utoronto.ca>
2026-07-23 18:36:37 -0400

    Added set up view and imports

---
+++ src/main/java/view/SetupView.java
@@ -0,0 +1,37 @@
+package view;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+//import interface_adapter.SetupView.SignupController;
+//import interface_adapter.SetupView.SignupState;
+//import interface_adapter.SetupView.SignupViewModel;
+
+    /**
+     * The View for the SetUp Use Case.
+     */
+    public class SetupView{
+
+}
