ba0a014269df6b993f5a09dd9b73a7b69cf16140
John <jsgeneralusage@gmail.com>
2026-07-23 22:38:22 -0400

    Create JPanel templates for Fighter Creation

--- src/main/java/view/FighterCreationView.java
+++ src/main/java/view/FighterCreationView.java
@@ -1,26 +1,110 @@
 package view;
-
-
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 
 public class FighterCreationView extends JPanel {
     private final JLabel rerollsRemainingLabel;
+    private final JPanel fighterPanel;
 
     public FighterCreationView() {
         setLayout(new BorderLayout(20, 0));
         setBackground(Color.white);
 
-//        rerollsRemainingLabel = createRerollsRemainingLabel();
-//
-//        add(rerollsRemainingLabel, BorderLayout.NORTH);
-//    }
+        fighterPanel = createYourFighterPanel();
+        add(fighterPanel, BorderLayout.WEST);
 
-
-    private JPanel createYourFighterPanel() {
-
+        rerollsRemainingLabel = createRerollsRemainingLabel();
+        add(rerollsRemainingLabel, BorderLayout.NORTH);
     }
+
+    /**
+     * Creates the left side of the fighter creation screen.
+     * This contains the stats of the player's fighter and progress.
+     */
+    private JPanel createYourFighterPanel() {
+        JPanel panel = new JPanel();
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        return panel;
+    }
+
+    private JPanel createFighterProgressPanel()  {
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+    private JPanel createAttributeTablePanel() {
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+    private JPanel createAttributePanel() {
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+    /**
+     * Creates the fighter draft panel on the right side of the screen.
+     * The player spins the for a fighter and assigns their attributes
+     */
+    private JPanel createFighterDraftPanel() {
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+    private JPanel createDraftHeaderPanel(){
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+    private JPanel createAttributeHeaderPanel() {
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+    private JPanel CreatedraftContentPanel(){
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+    private JPanel createPreRollPanel() {
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+    private JPanel createFighterCardPanel() {
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+    private JPanel createFighterHeaderPanel() {
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+    private JPanel createFighterStatsTablePanel() {
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+    private JPanel createFighterStatsPanel() {
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+    private JPanel createBuildActionsPanel() {
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+
+
+
+
+
+
 
 
 
