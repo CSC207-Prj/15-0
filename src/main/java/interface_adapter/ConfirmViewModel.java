b3ccf03bbbdc00b884265db875b93dbbfba448ed
Chetanya Moudgil <chetanya.moudgil@mail.utoronto.ca>
2026-08-08 19:34:24 -0400

    Added the state and viewmodel for user story, with clean architure and updates

--- src/main/java/interface_adapter/ConfirmViewModel.java
+++ src/main/java/interface_adapter/ConfirmViewModel.java
@@ -1,4 +1,6 @@
 package interface_adapter;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 public class ConfirmViewModel{ //extends Viewmodel<Confirm>{
     public static final String VIEW_NAME = "Confirm Fighter";
@@ -9,8 +11,34 @@ public class ConfirmViewModel{ //extends Viewmodel<Confirm>{
     public static final String SPIN_BUTTON_LABEL = "Spin Fighter Weight Class";
     public static final String CONFIRM_BUTTON_LABEL = "Confirm Fighter";
 
-    public ConfirmViewModel(){
-        //super(VIEW_NAME);
-        //setstate(new ConfirmState());
+    private final PropertyChangeSupport support =
+            new PropertyChangeSupport(this);
+    private ConfirmState state = new ConfirmState();
+
+    public ConfirmState getState() {
+        return state;
+    }
+
+    public void setState(ConfirmState state) {
+        final ConfirmState oldState = this.state;
+        if (state == null) {
+            this.state = new ConfirmState();
+        } else {
+            this.state = state;
+        }
+        support.firePropertyChange("state", oldState, this.state);
+    }
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        support.removePropertyChangeListener(listener);
     }
 }
+
