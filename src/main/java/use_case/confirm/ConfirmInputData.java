5f543131a84f9d21f1edc4a147f4bc9d3dadcd6b
Chetanya Moudgil <chetanya.moudgil@mail.utoronto.ca>
2026-08-09 00:56:28 -0400

    Added confirm input and output data

--- src/main/java/use_case/confirm/ConfirmInputData.java
+++ src/main/java/use_case/confirm/ConfirmInputData.java
@@ -1,4 +1,28 @@
 package use_case.confirm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConfirmInputData {
+    private String fighterName;
+    private List<String> attributePoints;
+    private String weightClass;
+
+    public ConfirmInputData(String fighterName, List<String> attributePoints, String weightClass) {
+        this.fighterName = fighterName;
+        if (attributePoints == null) {
+            this.attributePoints = new ArrayList<>();
+        }
+        this.attributePoints = new ArrayList<>(attributePoints);
+        this.weightClass = weightClass;
+    }
+    public String getFighterName() {
+        return fighterName;
+    }
+    public List<String> getAttributePoints() {
+        return new ArrayList<>(attributePoints);
+    }
+    public String getWeightClass() {
+        return weightClass;
+    }
 }
